5ed3224acc2e98db7cf053a065f60bbba1e9ebdb
César de la Rosa <code@cesardelarosa.xyz>
2026-07-23 16:02:22 +0200

    bitwise

--- src/Bitwise.hs
+++ src/Bitwise.hs
@@ -1,13 +1,19 @@
-module Bitwise (adder, multiplier) where
+module Bitwise (adder, multiplier, grayCode) where
 
 import Data.Word
 import Data.Bits
 
 adder :: Word32 -> Word32 -> Word32
 adder a 0 = a
-adder a b = adder (xor a b) $ shiftL (a .&. b) 1
+adder a b = (a `xor` b) `adder` shiftL (a .&. b) 1
 
 multiplier :: Word32 -> Word32 -> Word32
-multiplier _ 0 = 0
-multiplier a 1 = a
-multiplier a b = adder (multiplier a $ b .&. 1) $ multiplier (shiftL a 1) (shiftR b 1)
+multiplier a b = go a b 0
+    where
+        go _ 0 acc = acc
+        go x y acc
+            | y .&. 1 == 1  = go (shiftL x 1) (shiftR y 1) (adder acc x)
+            | otherwise     = go (shiftL x 1) (shiftR y 1) acc
+
+grayCode :: Word32 -> Word32
+grayCode a = a `xor` shiftR a 1
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,23 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Main (main) where
 
+import Data.Word
+import Data.Bits
+import Test.QuickCheck
+import System.Exit
+
 import Bitwise
-import Data.Word (Word32)
-import Test.QuickCheck (quickCheckAll, Property, (===))
-import System.Exit (exitFailure)
 
 prop_adder :: Word32 -> Word32 -> Property
 prop_adder a b = adder a b === a + b
 
 prop_multiplier :: Word32 -> Word32 -> Property
 prop_multiplier a b = multiplier a b === a * b
+
+prop_graycode :: Word32 -> Property
+prop_graycode n
+  | n == 0      = grayCode 0 === 0
+  | otherwise   = popCount (grayCode n `xor` grayCode (n - 1)) === 1
 
 return []
 
