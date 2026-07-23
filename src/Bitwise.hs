f177462707db4a95862a04d772343450c7f3880c
César de la Rosa <code@cesardelarosa.xyz>
2026-07-23 13:32:51 +0200

    clean useful tests, nice add, correct mult but not very efficient

--- src/Bitwise.hs
+++ src/Bitwise.hs
@@ -1,9 +1,13 @@
 module Bitwise (adder, multiplier) where
 
 import Data.Word
+import Data.Bits
 
 adder :: Word32 -> Word32 -> Word32
-adder a b = a + b
+adder a 0 = a
+adder a b = adder (xor a b) $ shiftL (a .&. b) 1
 
 multiplier :: Word32 -> Word32 -> Word32
-multiplier a b = a * b
+multiplier _ 0 = 0
+multiplier a 1 = a
+multiplier a b = adder (multiplier a $ b .&. 1) $ multiplier (shiftL a 1) (shiftR b 1)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,17 +1,22 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main (main) where
 
-import Bitwise (adder, multiplier)
+import Bitwise
 import Data.Word (Word32)
-import Test.QuickCheck (quickCheck)
+import Test.QuickCheck (quickCheckAll, Property, (===))
+import System.Exit (exitFailure)
 
-prop_adder :: Word32 -> Word32 -> Bool
-prop_adder a b = adder a b == a + b
+prop_adder :: Word32 -> Word32 -> Property
+prop_adder a b = adder a b === a + b
 
-prop_multiplier :: Word32 -> Word32 -> Bool
-prop_multiplier a b = multiplier a b == a * b
+prop_multiplier :: Word32 -> Word32 -> Property
+prop_multiplier a b = multiplier a b === a * b
+
+return []
 
 main :: IO ()
 main = do
-    putStrLn "--- BITWISE TESTS ---"
-    quickCheck prop_adder
-    quickCheck prop_multiplier
+    success <- $quickCheckAll
+    if success
+        then return ()
+        else exitFailure
