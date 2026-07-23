9c0f063bd373f934cfad57f6745797c6669cf59b
César de la Rosa <code@cesardelarosa.xyz>
2026-07-23 16:18:02 +0200

    cleaner mult

--- src/Bitwise.hs
+++ src/Bitwise.hs
@@ -11,9 +11,10 @@ multiplier :: Word32 -> Word32 -> Word32
 multiplier a b = go a b 0
     where
         go _ 0 acc = acc
-        go x y acc
-            | y .&. 1 == 1  = go (shiftL x 1) (shiftR y 1) (adder acc x)
-            | otherwise     = go (shiftL x 1) (shiftR y 1) acc
+        go x y acc = go (shiftL x 1) (shiftR y 1) nextAcc
+            where
+                nextAcc | y .&. 1 == 1 = adder acc x
+                        | otherwise    = acc
 
 grayCode :: Word32 -> Word32
 grayCode a = a `xor` shiftR a 1
